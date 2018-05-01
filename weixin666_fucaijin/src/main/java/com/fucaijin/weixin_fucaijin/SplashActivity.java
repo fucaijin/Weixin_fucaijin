494236d29d0c5d4e97c2a7110ef711cf20fa9f33
fucaijin <fucaijin@qq.com>
2018-05-01 11:53:58 +0800

    Initial commit

---
+++ weixin666_fucaijin/src/main/java/com/fucaijin/weixin_fucaijin/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.fucaijin.weixin_fucaijin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
