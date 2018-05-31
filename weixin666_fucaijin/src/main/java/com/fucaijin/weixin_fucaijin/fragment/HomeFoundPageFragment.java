633e279fbcb083a3a3abc919b448127d57a59892
fucaijin <fucaijin@qq.com>
2018-05-31 23:12:27 +0800

    finish found fragment and me fragment layout 完成发现页面和我的页面的布局

--- weixin666_fucaijin/src/main/java/com/fucaijin/weixin_fucaijin/fragment/HomeFoundPageFragment.java
+++ weixin666_fucaijin/src/main/java/com/fucaijin/weixin_fucaijin/fragment/HomeFoundPageFragment.java
@@ -7,19 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fucaijin.weixin_fucaijin.R;
-import com.fucaijin.weixin_fucaijin.adapter.HomeFragmentFoundListViewAdapter;
 
 /**
  * Created by fucaijin on 2018/5/9.
  */
 
 public class HomeFoundPageFragment extends Fragment implements AdapterView.OnItemClickListener {
-//    TODO ListView没有实现点击的Selector效果，原因：Item设置了backGround，ListVie的Selector不起作用
+//    TODO 需要根据设置来设定条目是否隐藏，并且如果之前的条目隐藏，当前条目是否要增加MarginTop。读取配置文件，获取显示的array,判断当前哪些条目显示
     int[] foundIconArray;
     String[] foundTextArray;
 
@@ -49,9 +47,6 @@ public class HomeFoundPageFragment extends Fragment implements AdapterView.OnIte
         }
 
         View foundFragmentLayout = inflater.inflate(R.layout.home_fragment_found, container, false);
-        ListView homeFragmentFoundLv = (ListView) foundFragmentLayout.findViewById(R.id.home_fragment_found_lv);
-        homeFragmentFoundLv.setOnItemClickListener(this);
-        homeFragmentFoundLv.setAdapter(new HomeFragmentFoundListViewAdapter(foundIconArray,foundTextArray));
         return foundFragmentLayout;
     }
 
--- weixin666_fucaijin/src/main/java/com/fucaijin/weixin_fucaijin/fragment/HomeFragmentMe.java
+++ weixin666_fucaijin/src/main/java/com/fucaijin/weixin_fucaijin/fragment/HomeFragmentMe.java
@@ -14,6 +14,7 @@ import com.fucaijin.weixin_fucaijin.R;
  */
 
 public class HomeFragmentMe extends Fragment {
+//    TODO 需要给各个条目添加ID，并找到，然后实现各个点击事件
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
